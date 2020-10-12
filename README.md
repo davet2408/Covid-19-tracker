99258ddb9cfb246fae825e9a857d08bdf112dab7
David Temple <david.temple@kcl.ac.uk>
2020-10-12 13:24:56 +0100

    Add api calls for country dropdown component

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Covid-19 Tracker App
-React app for tracking and displaying global data on the Covid-19 pandemic.
+React app for tracking and displaying global data on the Covid-19 pandemic 
+using data from the https://disease.sh/ v3 api.
 
 
--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
+import React, { useState, useEffect } from 'react';
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import React from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country, // United States, United Kingdom etc
+              value: country.countryInfo.iso2 // UK, USA, FR
+            }));
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    console.log("****", countryCode);
+
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
-      <h1>Covid-19 Tracker</h1>
-      <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="World Wide">op1</MenuItem>
-          <MenuItem value="UK">op2</MenuItem>
-          <MenuItem value="USA">op3</MenuItem>
-        </Select>
-      </FormControl>
-
-      {/* {Header} */}
-      {/* {title + select inout dropdown field} */}
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* {InfoBoxs} */}
       {/* {InfoBoxs} */}
