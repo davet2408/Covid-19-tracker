b1e728c6a999782a00bcfd6a15958957f5352391
David Temple <david.temple@kcl.ac.uk>
2020-10-13 08:26:11 +0100

    Add Table with sorted worldwide cases

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Card, FormControl, MenuItem, Select, CardContent } from '@material-ui/core';
-import InfoBox from './InfoBox'
-import Map from './Map'
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
 import './App.css';
+import { sortData } from './utils'
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all')
@@ -27,6 +30,9 @@ function App() {
               name: country.country, // United States, United Kingdom etc
               value: country.countryInfo.iso2 // UK, USA, FR
             }));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -76,7 +82,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3>Live Cases by country</h3>
-          {/* {Table} */}
+          <Table countries={tableData} />
           {/* {Graph} */}
         </CardContent>
 
